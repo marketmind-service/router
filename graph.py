ef5d4a2d91b015ae9916233e29ec33e900f290c6
Gary <ggao44@uwo.ca>
2025-11-23 18:47:36 -0500

    set up repo

--- graph.py
+++ graph.py
@@ -1,9 +1,8 @@
 from langgraph.graph import StateGraph, END
-from backend.state import AgentState
-from backend.agent.prompt_classification import classify_prompt
-from backend.agent.router import router
+from state import AgentState
+from prompt_classification import classify_prompt
+from router import router
 
-from backend.lookup.lookup_agent import lookup_agent
 
 
 def create_agent_graph():
@@ -12,16 +11,18 @@ def create_agent_graph():
 
     graph.add_node("classify_prompt", classify_prompt)
     graph.add_node("router", router)
-    graph.add_node("lookup_agent", lookup_agent)
+    #graph.add_node("lookup_agent", lookup_agent)
 
     graph.add_edge("classify_prompt", "router")
 
-    graph.add_conditional_edges(
-        "router",
-        lambda state: (
-            "lookup_agent" if state.route_plan and state.route_plan[0] == "stock_lookup" else
-            END
-        )
-    )
+    #graph.add_conditional_edges(
+    #    "router",
+    #    lambda state: (
+    #        "lookup_agent" if state.route_plan and state.route_plan[0] == "stock_lookup" else
+    #        END
+    #    )
+    #)
+
+    graph.add_edge("router", END)
 
     return graph.compile()
