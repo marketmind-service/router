76ff927195eaeae603e509ee987407886f35e247
Gary <ggao44@uwo.ca>
2025-11-23 20:12:20 -0500

    set up repo

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 from langchain_openai import AzureChatOpenAI
 
 query = AzureChatOpenAI(
-    azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT"],
     azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT"],
     api_version="2025-08-07",
-    temperature=0,
 )
